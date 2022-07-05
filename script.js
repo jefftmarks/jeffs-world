5536926d393ec31718bf36e3a5ec658f58a6f02e
Jeff Marks <jeff.t.marks@gmail.com>
2022-07-05 12:07:07 -0400

    added 'Click Me!' eventListener in navbar

--- script.js
+++ script.js
@@ -5,26 +5,32 @@ const games = document.getElementById("games");
 bio.addEventListener("mouseover", function() {
     gators.style.color="#fff";
     games.style.color="#fff";
+    bio.textContent="Click Me!";
 });
 bio.addEventListener("mouseout", function() {
     gators.style.color="#0000EE";
     games.style.color="#0000EE";
+    bio.textContent="Bio";
 });
 
 gators.addEventListener("mouseover", function() {
     bio.style.color="#fff";
     games.style.color="#fff";
+    gators.textContent="Click Me!";
 });
 gators.addEventListener("mouseout", function() {
     bio.style.color="#0000EE";
     games.style.color="#0000EE";
+    gators.textContent="Gators";
 });
 
 games.addEventListener("mouseover", function() {
     bio.style.color="#fff";
     gators.style.color="#fff";
+    games.textContent="Click Me!";
 });
 games.addEventListener("mouseout", function() {
     bio.style.color="#0000EE";
     gators.style.color="#0000EE";
+    games.textContent="Games!";
 });
