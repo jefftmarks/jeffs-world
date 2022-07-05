8bcad7a9ef52236e9c834f0b794fc422b5f13124
Jeff Marks <jeff.t.marks@gmail.com>
2022-07-05 12:31:44 -0400

    finish navbar

--- script.js
+++ script.js
@@ -5,32 +5,38 @@ const games = document.getElementById("games");
 bio.addEventListener("mouseover", function() {
     gators.style.color="#fff";
     games.style.color="#fff";
-    bio.textContent="Click Me!";
+    bio.style.color="#ff0000";
+    bio.textContent="CLICK";
 });
 bio.addEventListener("mouseout", function() {
-    gators.style.color="#0000EE";
-    games.style.color="#0000EE";
+    gators.style.color="#000";
+    games.style.color="#000";
+    bio.style.color="#000";
     bio.textContent="Bio";
 });
 
 gators.addEventListener("mouseover", function() {
     bio.style.color="#fff";
     games.style.color="#fff";
-    gators.textContent="Click Me!";
+    gators.style.color="#ff0000";
+    gators.textContent="CLICK";
 });
 gators.addEventListener("mouseout", function() {
-    bio.style.color="#0000EE";
-    games.style.color="#0000EE";
+    bio.style.color="#000";
+    games.style.color="#000";
+    gators.style.color="#000"
     gators.textContent="Gators";
 });
 
 games.addEventListener("mouseover", function() {
     bio.style.color="#fff";
     gators.style.color="#fff";
-    games.textContent="Click Me!";
+    games.style.color="#ff0000";
+    games.textContent="CLICK";
 });
 games.addEventListener("mouseout", function() {
-    bio.style.color="#0000EE";
-    gators.style.color="#0000EE";
-    games.textContent="Games!";
+    bio.style.color="#000";
+    gators.style.color="#000";
+    games.style.color="#000";
+    games.textContent="Games";
 });
